2ccba7cc7306af6c1042fb3f6b810917338ce44a
matt <matt.ho>
2014-04-21 15:44:10 -0700

    - updated default DOCKER_REGISTRY

--- tasks/env.rb
+++ tasks/env.rb
@@ -1,7 +1,7 @@
 APP             = 'sample/app'
 BUILD_NUMBER    = ENV["BUILD_NUMBER"] || 'latest'
 
-DOCKER_REGISTRY = 'ec2-54-188-129-225.us-west-2.compute.amazonaws.com:5000'
+DOCKER_REGISTRY = 'ec2-184-72-21-165.us-west-1.compute.amazonaws.com:5000'
 DOCKER_USERNAME = ENV["DOCKER_USERNAME"] || "admin"
 DOCKER_PASSWORD = ENV["DOCKER_PASSWORD"] || "password"
 DOCKER_EMAIL    = ENV["DOCKER_EMAIL"]    || "docker@loyal3.com"
